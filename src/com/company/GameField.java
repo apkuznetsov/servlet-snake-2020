fb101ca8b85d01ce636721aa5b33cc2dfcdbd612
Andrey <kouznts@ya.ru>
2020-03-07 21:42:17 +0400

    поля GameField

---
+++ src/com/company/GameField.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.Time;
+
+public class GameField extends JPanel {
+    private final int SIZE = 320;
+    private final int DOT_SIZE = 16;
+    private final int ALL_DOTS = (int)Math.pow(SIZE/DOT_SIZE, 2);
+
+    private Image dot;
+    private Image apple;
+
+    private int appleX;
+    private int appleY;
+
+    private int[] snakeX = new int[ALL_DOTS];
+    private int[] snakeY = new int[ALL_DOTS];
+
+    private int snakeSize;
+
+    private Time timer;
+
+    private boolean left = false;
+    private boolean right = true;
+    private boolean up = false;
+    private boolean down = false;
+    private boolean isInGame = true;
+
+    public GameField() {
+
+    }
+}
