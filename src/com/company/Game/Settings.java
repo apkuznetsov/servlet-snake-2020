ee27ec04877b8a54467ef9d92b649efb2dc88abf
Andrey <kouznts@ya.ru>
2020-03-08 11:33:56 +0400

    класс Settings

---
+++ src/com/company/Game/Settings.java
@@ -0,0 +1,12 @@
+package com.company.Game;
+
+public final class Settings {
+    public static final String TITLE = "Змейка";
+
+    public static final int WINDOW_SIZE = 350;
+    public static final int DOT_SIZE = 16;
+    public static final int ALL_DOTS = (int) Math.pow(WINDOW_SIZE / DOT_SIZE, 2);;
+
+    public static final String APPLE_IMAGE = "apple.png";
+    public static final String SNAKE_DOT_IMAGE = "snake-dot.png";
+}
