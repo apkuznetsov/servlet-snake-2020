f0ab3d91a9c15cb66c47a00746031775e79f8ca0
Andrey <kouznts@ya.ru>
2020-03-08 20:32:44 +0400

    увеличить размер окна в два раза

--- src/com/company/SnakeGame/Settings.java
+++ src/com/company/SnakeGame/Settings.java
@@ -5,7 +5,7 @@ import org.jetbrains.annotations.NotNull;
 public final class Settings {
     // region статика
     public static final int DOT_SIZE = 16;
-    public static final int DOTS_NUMBER_PER_DIMENSION = 20;
+    public static final int DOTS_NUMBER_PER_DIMENSION = 40;
 
     public static final String APPLE_IMAGE_LOCATION = "apple.png";
     public static final String SNAKE_DOT_IMAGE_LOCATION = "snake-dot.png";
